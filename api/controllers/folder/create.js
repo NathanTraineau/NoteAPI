96278f8a03821e0ac37b12ecf9c775ac85515d27
NathanTraineau <nathan.trn@orange.fr>
2020-04-19 15:37:16 +0200

    deploy

--- api/controllers/folder/create.js
+++ api/controllers/folder/create.js
@@ -53,8 +53,9 @@ module.exports = {
   fn: async function ({title, type, parent}, exits) {
 
     const { user } = this.req;
-
+    console.log('hhhhhhhhhhhhhhhh')
     if(parent){
+      console.log('parent' + parent)
       let folderItem = await Folder.findOne({id: parent});
 
       if(!folderItem) {
